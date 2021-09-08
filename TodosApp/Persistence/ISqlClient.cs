3539fc9c29c36f03315d5a9b0e8d0367fc25522a
Gintautas <gintautas.pusinskas@gmail.com>
2021-09-08 22:50:36 +0300

    Tailored some methods for new client interface

---
+++ TodosApp/Persistence/ISqlClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public interface ISqlClient
+    {
+        Task<int> ExecuteAsync(string sql, object param = null);
+
+        Task<IEnumerable<T>> QueryAsync<T>(string sql, object param = null);
+
+        //  Task<T> QueryFirstOrDefaultAsync<T>(string sql, object param = null);
+    }
+}
---
+++ TodosApp/Persistence/SqlClient.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    internal class SqlClient : ISqlClient
+    {
+        private readonly string _connectionString;
+
+        public SqlClient(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<int> ExecuteAsync(string sql, object param = null)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+
+            var rowsAffected = await connection.ExecuteAsync(sql, param);
+            if (rowsAffected < 1)
+            {
+                throw new Exception("No rows affected");
+            }
+
+            return rowsAffected;
+        }
+
+        public Task<IEnumerable<T>> QueryAsync<T>(string sql, object param = null)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            return connection.QueryAsync<T>(sql, param);
+        }
+
+        /*public Task<T> QueryFirstOrDefaultAsync<T>(string sql, object param = null)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            return connection.QueryFirstOrDefaultAsync<T>(sql, param);
+        }*/
+    }
+}
